6169b11d97a794e0f3098b3accedd4abe2a23c8e
DESKTOP-FAB0NJ2\jumi6 <jumi6827@naver.com>
2021-06-27 23:44:18 +0900

    가게 선택하는 액티비티에 시작 음성 안내까지!

--- client/blindkiosk/app/src/main/java/com/abc/blindkiosk/MainActivity.java
+++ client/blindkiosk/app/src/main/java/com/abc/blindkiosk/MainActivity.java
@@ -2,13 +2,25 @@ package com.abc.blindkiosk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intent = new Intent(this, StoreActivity.class);
+        Button btnStore = (Button) findViewById(R.id.btnStore);
+        btnStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
--- client/blindkiosk/app/src/main/java/com/abc/blindkiosk/StoreActivity.java
+++ client/blindkiosk/app/src/main/java/com/abc/blindkiosk/StoreActivity.java
@@ -3,12 +3,39 @@ package com.abc.blindkiosk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Locale;
 
 public class StoreActivity extends AppCompatActivity {
-
+    TextToSpeech textToSpeech;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+
+
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status==TextToSpeech.SUCCESS){
+                    textToSpeech.setLanguage(Locale.KOREAN);
+                    findUserLocation();
+                }
+            }
+        });
+    }
+
+
+    void findUserLocation(){
+        textToSpeech.speak("사용자의 위치를 탐색중입니다.",TextToSpeech.QUEUE_ADD,null);
+
+        chooseStore();
+    }
+
+    void chooseStore(){
+        textToSpeech.speak("가게를 선택해주세요.",TextToSpeech.QUEUE_ADD,null);
     }
 }

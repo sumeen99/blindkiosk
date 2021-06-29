d90ccb44775c47d07726c5e9d6b230877aa7a968
DESKTOP-FAB0NJ2\jumi6 <jumi6827@naver.com>
2021-06-29 23:26:58 +0900

    음성인식 하라는대로 했는데 왜 안 되는지 의문,,

--- client/blindkiosk/app/src/main/java/com/abc/blindkiosk/StoreActivity.java
+++ client/blindkiosk/app/src/main/java/com/abc/blindkiosk/StoreActivity.java
@@ -7,66 +7,73 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class StoreActivity extends AppCompatActivity {
     TextToSpeech textToSpeech;
-    Button btnGPS;
     TextView position;
 
+    Button btnSpeech;
+    SpeechRecognizer mRecognizer;
+    String storeNumber;
+    TextView answer;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
 
-        position = (TextView)findViewById(R.id.position);
-        btnGPS = (Button) findViewById(R.id.btnGPS);
+        btnSpeech = (Button)findViewById(R.id.btnSpeech);
+
+        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");
+        answer = (TextView)findViewById((R.id.answer));
+
+        position = (TextView) findViewById(R.id.position);
         textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if (status == TextToSpeech.SUCCESS) {
                     textToSpeech.setLanguage(Locale.KOREAN);
-                    findUserLocation(getApplicationContext());
+                    findUserLocation();
                 }
             }
         });
+
 
     }
 
 
-    void findUserLocation(final Context context) {
-        textToSpeech.speak("사용자의 위치를 탐색합니다.화면을 눌러주세요.", TextToSpeech.QUEUE_ADD, null);
-
+    void findUserLocation() {
+        textToSpeech.speak("사용자의 위치를 탐색합니다.", TextToSpeech.QUEUE_ADD, null);
 
         final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 1, mLocationListener);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 100, 1, mLocationListener);
+        } catch (SecurityException ex) {
+            locationManager.removeUpdates(mLocationListener);
+        }
 
-
-        btnGPS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try{
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,100,1,mLocationListener);
-                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,100,1,mLocationListener);
-                }catch (SecurityException ex){
-                    locationManager.removeUpdates(mLocationListener);
-                }
-
-            }
-        });
-
-
-        //chooseStore();
+        chooseStore();
     }
 
 
@@ -80,7 +87,67 @@ public class StoreActivity extends AppCompatActivity {
             position.setText("위치정보 : " + provider + "\n" + "위도 : " + latitude + "\n" + "경도 : " + longitude + "\n" + "고도 : " + altitude);
         }
     };
-    void chooseStore(){
-        textToSpeech.speak("가게를 선택해주세요.",TextToSpeech.QUEUE_ADD,null);
+
+    void chooseStore() {
+        textToSpeech.speak("화면을 눌러 가게 번호를 말씀해주세요.", TextToSpeech.QUEUE_ADD, null);
+
+        btnSpeech.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mRecognizer = SpeechRecognizer.createSpeechRecognizer(StoreActivity.this);
+                mRecognizer.setRecognitionListener(listener);
+                mRecognizer.startListening(intent);
+            }
+        });
     }
+
+    private RecognitionListener listener = new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            Toast.makeText(getApplicationContext(),"음성인식을 시작합니다.",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB) {
+
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+
+        }
+
+        @Override
+        public void onError(int error) {
+
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for(int i=0;i<matches.size();i++){
+                answer.setText(matches.get(i));
+            }
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+
+        }
+    };
 }

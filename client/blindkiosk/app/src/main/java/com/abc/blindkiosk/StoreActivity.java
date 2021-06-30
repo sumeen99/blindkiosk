7bf569193c52a09adb8a5a812ada1c083a215df1
DESKTOP-FAB0NJ2\jumi6 <jumi6827@naver.com>
2021-06-30 17:46:31 +0900

    음성인식 완료!

--- client/blindkiosk/app/src/main/java/com/abc/blindkiosk/StoreActivity.java
+++ client/blindkiosk/app/src/main/java/com/abc/blindkiosk/StoreActivity.java
@@ -35,45 +35,49 @@ public class StoreActivity extends AppCompatActivity {
     String storeNumber;
     TextView answer;
     Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
 
-        btnSpeech = (Button)findViewById(R.id.btnSpeech);
-
-        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName());
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");
-        answer = (TextView)findViewById((R.id.answer));
-
+        btnSpeech = (Button) findViewById(R.id.btnSpeech);
         position = (TextView) findViewById(R.id.position);
-        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if (status == TextToSpeech.SUCCESS) {
-                    textToSpeech.setLanguage(Locale.KOREAN);
-                    findUserLocation();
-                }
-            }
-        });
+        answer = (TextView) findViewById((R.id.answer));
 
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(StoreActivity.this, new String[]{ Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        } else {
+            intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");
+
+            textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+                @Override
+                public void onInit(int status) {
+                    if (status == TextToSpeech.SUCCESS) {
+                        textToSpeech.setLanguage(Locale.KOREAN);
+                        findUserLocation();
+                    }
+                }
+            });
+            Toast.makeText(getApplicationContext(), "퍼미션 체크 완료.", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
 
     void findUserLocation() {
         textToSpeech.speak("사용자의 위치를 탐색합니다.", TextToSpeech.QUEUE_ADD, null);
-
         final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        try {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 1, mLocationListener);
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 100, 1, mLocationListener);
-        } catch (SecurityException ex) {
-            locationManager.removeUpdates(mLocationListener);
-        }
+            try {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 1, mLocationListener);
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 100, 1, mLocationListener);
+                chooseStore();
+            }catch (SecurityException ex){
 
-        chooseStore();
+            }
+
     }
 
 
@@ -85,17 +89,28 @@ public class StoreActivity extends AppCompatActivity {
             double latitude = location.getLatitude();
             double altitude = location.getAltitude();
             position.setText("위치정보 : " + provider + "\n" + "위도 : " + latitude + "\n" + "경도 : " + longitude + "\n" + "고도 : " + altitude);
+            Toast.makeText(getApplicationContext(), "위치정보 입력.", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderEnabled(@NonNull String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(@NonNull String provider) {
+
         }
     };
 
     void chooseStore() {
         textToSpeech.speak("화면을 눌러 가게 번호를 말씀해주세요.", TextToSpeech.QUEUE_ADD, null);
 
-        btnSpeech.setOnClickListener(new View.OnClickListener(){
+        btnSpeech.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mRecognizer = SpeechRecognizer.createSpeechRecognizer(StoreActivity.this);
-                mRecognizer.setRecognitionListener(listener);
+                mRecognizer = SpeechRecognizer.createSpeechRecognizer(StoreActivity.this); //
+                mRecognizer.setRecognitionListener(listener);   //모든 콜백을 수신하는 리스너 설정
                 mRecognizer.startListening(intent);
             }
         });
@@ -104,7 +119,7 @@ public class StoreActivity extends AppCompatActivity {
     private RecognitionListener listener = new RecognitionListener() {
         @Override
         public void onReadyForSpeech(Bundle params) {
-            Toast.makeText(getApplicationContext(),"음성인식을 시작합니다.",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "음성인식을 시작합니다.", Toast.LENGTH_SHORT).show();
         }
 
         @Override
@@ -129,13 +144,52 @@ public class StoreActivity extends AppCompatActivity {
 
         @Override
         public void onError(int error) {
+            String message;
+
+            switch (error) {
+                case SpeechRecognizer.ERROR_AUDIO:
+                    message = "오디오 에러";
+                    break;
+                case SpeechRecognizer.ERROR_CLIENT:
+                    message = "클라이언트 에러";
+                    break;
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    message = "퍼미션 없음";
+                    textToSpeech.speak("설정에서 마이크 권한을 허용해주세요.", TextToSpeech.QUEUE_ADD, null);
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK:
+                    message = "네트워크 에러";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                    message = "네트웍 타임아웃";
+                    break;
+                case SpeechRecognizer.ERROR_NO_MATCH:
+                    message = "찾을 수 없음";
+                    break;
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    message = "RECOGNIZER가 바쁨";
+                    textToSpeech.speak("잠시 후에 다시 말씀해주세요.", TextToSpeech.QUEUE_ADD, null);
+                    break;
+                case SpeechRecognizer.ERROR_SERVER:
+                    message = "서버가 이상함";
+                    break;
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                    message = "말하는 시간초과";
+                    textToSpeech.speak("말하는 시간이 초과되었습니다. 화면을 다시 눌러 가게 번호를 말씀해주세요.", TextToSpeech.QUEUE_ADD, null);
+                    break;
+                default:
+                    message = "알 수 없는 오류임";
+                    break;
+            }
+
+            Toast.makeText(getApplicationContext(), "에러가 발생하였습니다. : " + message, Toast.LENGTH_SHORT).show();
 
         }
 
         @Override
         public void onResults(Bundle results) {
             ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-            for(int i=0;i<matches.size();i++){
+            for (int i = 0; i < matches.size(); i++) {
                 answer.setText(matches.get(i));
             }
         }
